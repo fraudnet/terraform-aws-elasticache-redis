c5e1e7a69efaca87ec2996420b9ec0becb19af44
Jacek <jacek@ivolution.pl>
2019-04-17 16:38:21 +0200

    outputs

--- outputs.tf
+++ outputs.tf
@@ -6,20 +6,20 @@ output "elasticache_replication_group_id" {
 output "elasticache_replication_group_primary_endpoint_address" {
   # value       = "${ join("", aws_elasticache_replication_group.default.primary_endpoint_address) }" # conditional output hack
 
-  value = "${compact(concat(list(var.replicas_per_node_group == "0" ? "" : "Not set"), aws_elasticache_replication_group.default.primary_endpoint_address))}"
+  value = "${element(compact(concat(list(var.replicas_per_node_group == "0" ? "" : "Not set"), list(aws_elasticache_replication_group.default.primary_endpoint_address))), 0)}"
   # value = "${element(concat(foo, list("default"), 0)}"
   description = "The address of the endpoint for the primary node in the replication group."
 }
 
 output "elasticache_replication_group_member_clusters" {
   # value       = "${ join("", aws_elasticache_replication_group.default.member_clusters) }"  # conditional output hack
-  value = "${compact(concat(list(var.replicas_per_node_group == "0" ? "" : "Not set"), aws_elasticache_replication_group.default.member_clusters))}"
+  value = "${element(compact(concat(list(var.replicas_per_node_group == "0" ? "" : "Not set"), list(aws_elasticache_replication_group.default.member_clusters))), 0)}"
   description = "The identifiers of all the nodes that are part of this replication group."
 }
 
 output "elasticache_replication_group_configuration_endpoint_address" {
   # value       = "${ join("", list(aws_elasticache_replication_group.default.configuration_endpoint_address)) }"   # conditional output hack
-  value = "${compact(concat(list(var.replicas_per_node_group != "0" ? "" : "Not set"), aws_elasticache_replication_group.default.configuration_endpoint_address))}"
+  value = "${element(compact(concat(list(var.replicas_per_node_group != "0" ? "" : "Not set"), list(aws_elasticache_replication_group.default.configuration_endpoint_address))), 0)}"
   description = "The address of the replication group configuration endpoint when cluster mode is enabled."
 }
 
