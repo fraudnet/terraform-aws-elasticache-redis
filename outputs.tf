4022bfe0604c5c139f9ce37430ac595916a65410
Jacek <jacek@ivolution.pl>
2019-04-17 17:15:30 +0200

    unable to conditionally return primary_endpoint_address

--- outputs.tf
+++ outputs.tf
@@ -3,15 +3,15 @@ output "elasticache_replication_group_id" {
   description = "The ID of the ElastiCache Replication Group."
 }
 
-output "elasticache_replication_group_primary_endpoint_address" {
-  # value       = "${ join("", aws_elasticache_replication_group.default.primary_endpoint_address) }" # conditional output hack
+# output "elasticache_replication_group_primary_endpoint_address" {
+#   # value       = "${ join("", aws_elasticache_replication_group.default.primary_endpoint_address) }" # conditional output hack
 
-  # value = "${element(compact(concat(list(var.replicas_per_node_group == "0" ? "" : "Not set"), list(aws_elasticache_replication_group.default.primary_endpoint_address ? aws_elasticache_replication_group.default.primary_endpoint_address :""))), 0)}"
+#   # value = "${element(compact(concat(list(var.replicas_per_node_group == "0" ? "" : "Not set"), list(aws_elasticache_replication_group.default.primary_endpoint_address ? aws_elasticache_replication_group.default.primary_endpoint_address :""))), 0)}"
 
-  # value = "${join("", aws_elasticache_replication_group.default.*.primary_endpoint_address)}"
-  value = "${aws_elasticache_replication_group.default.primary_endpoint_address}"
-  description = "The address of the endpoint for the primary node in the replication group."
-}
+#   # value = "${join("", aws_elasticache_replication_group.default.*.primary_endpoint_address)}"
+#   value = "${aws_elasticache_replication_group.default.primary_endpoint_address}"
+#   description = "The address of the endpoint for the primary node in the replication group."
+# }
 
 output "elasticache_replication_group_member_clusters" {
   # value       = "${ join("", aws_elasticache_replication_group.default.member_clusters) }"  # conditional output hack
